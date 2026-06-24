76df9604b1ca85d5ee5abd034aeec89a3a2677b5
GiantRiler <Rileyf257@outlook.com>
2026-06-23 21:54:35 -0600

    Learned how to make blocks

--- src/main/java/net/giantriler/rilersmod/RilerMod.java
+++ src/main/java/net/giantriler/rilersmod/RilerMod.java
@@ -1,6 +1,9 @@
 package net.giantriler.rilersmod;
 
 import com.mojang.logging.LogUtils;
+import net.giantriler.rilersmod.block.ModBlocks;
+import net.giantriler.rilersmod.item.ModCreativeModeTabs;
+import net.giantriler.rilersmod.item.ModItems;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.food.FoodProperties;
@@ -39,6 +42,13 @@ public class RilerMod
     public RilerMod()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModCreativeModeTabs.register(modEventBus);
+
+        ModItems.register(modEventBus);
+
+        ModBlocks.register(modEventBus);
+
         modEventBus.addListener(this::commonSetup);
         MinecraftForge.EVENT_BUS.register(this);
         modEventBus.addListener(this::addCreative);
@@ -52,7 +62,10 @@ public class RilerMod
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.SAPPHIRE);
+            event.accept(ModItems.RAW_SAPPHIRE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
